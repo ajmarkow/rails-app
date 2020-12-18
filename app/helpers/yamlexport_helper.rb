bb1e7f63e93ffbbfc149311bfba42b87d71992a8
AJ Markow <alexanderjmarkow@gmail.com>
2020-12-18 11:56:35 -0800

    Add yamlexport_helper file.  Add test.yml to test output.

---
+++ app/helpers/yamlexport_helper.rb
@@ -0,0 +1,12 @@
+require 'yaml'
+
+snippet = {'- trigger': ":thisismysnippet",
+             'replacement': "Isnt my snippet cool?" }
+
+  def yamlfileexport(snippet)
+    File.open("test.yml","w") { |file| file.write(snippet.to_yaml) }
+    puts "SAVING TO YAML NOW"
+  end
+
+  yamlfileexport(snippet)
+
